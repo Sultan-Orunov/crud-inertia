1c6c3eb441e43715ada7f613afa3bac74327c573
Sultan-Orunov <osbfswd@gmail.com>
2023-12-04 20:42:27 +0500

    добавил postController, начальный вывод постов на Index

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-for="post in posts" :key="post.id">
+        {{post.title}}
+    </div>
+</template>
+
+<script setup>
+defineProps(['posts'])
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return inertia('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,8 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/posts', \App\Http\Controllers\Post\IndexController::class)->name('post.index');
+
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return inertia('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
